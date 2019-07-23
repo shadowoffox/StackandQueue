e3be38f0b8e1d630862478c4e6858bedc3d1bed7
shadowoffox <denisochnev@mail.u>
2019-07-23 13:33:56 +0300

    Queue Stack and deque

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,75 @@
 import java.lang.reflect.Array;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        //Стэк
+    //Первое ну и второе задание
        /* MyStack<Integer> myStack = new MyStack<>();
+        System.out.println("введите 10 чисел через пробел");
+         for (int i = 0; i <10 ; i++) {
+            MyStack.push(in.nextInt());
+        }
 
-        myStack.push(1);
-        myStack.push(2);
-        myStack.push(3);
-        myStack.push(4);
-        myStack.push(5);
-        myStack.push(6);
-        myStack.push(7);
-        myStack.push(8);
-
-        for (int i = 0; i <8 ; i++) {
+        for (int i = 0; i <10 ; i++) {
             System.out.println(myStack.pop());
         }*/
 
-       MyQueue<Integer> myQueue = new MyQueue<>();
 
-       myQueue.insert(1);
-       myQueue.insert(2);
-       myQueue.insert(3);
-       myQueue.insert(4);
-       myQueue.insert(5);
-       myQueue.insert(6);
-       myQueue.insert(7);
-       myQueue.insert(8);
+    //Очередь
+   /*    MyQueue<Integer> myQueue = new MyQueue<>();
+        for (int i = 0; i <10 ; i++) {
+            myQueue.insert(in.nextInt());
+        }
         System.out.println(myQueue);
+
+
+        myQueue.remove();
+        System.out.println(myQueue);
+
+        myQueue.remove();
+        System.out.println(myQueue);*/
+
+//Двустороняя очередь(дек)
+      /*  MyDeck<Integer> myDeck = new MyDeck<>();
+
+        myDeck.push_front(1);
+        System.out.println(myDeck);
+        myDeck.push_front(2);
+        System.out.println(myDeck);
+        myDeck.push_front(3);
+        System.out.println(myDeck);
+        myDeck.push_front(4);
+        System.out.println(myDeck);
+        myDeck.push_front(5);
+        System.out.println(myDeck);
+        myDeck.push_end(6);
+        System.out.println(myDeck);
+        myDeck.push_end(7);
+        System.out.println(myDeck);
+        myDeck.push_end(8);
+        System.out.println(myDeck);
+        myDeck.push_end(9);
+        System.out.println(myDeck);
+        myDeck.push_end(10);
+
+        System.out.println(myDeck);
+
+
+        myDeck.pop_end();
+        System.out.println(myDeck);
+        myDeck.pop_front();
+        System.out.println(myDeck);
+
+        myDeck.push_end(88);
+        System.out.println(myDeck);
+
+        myDeck.push_front(33);
+        System.out.println(myDeck);
+*/
     }
 }
